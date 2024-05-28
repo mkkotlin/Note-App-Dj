a7be3d71c528a0be436f95476923673ce86bd944
Mayank Kumar <87866647+mkkotlin@users.noreply.github.com>
2024-05-28 07:37:23 +0000

    commited

--- NoteApp/settings.py
+++ NoteApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q-)25%j#&+1776c@r&e+4=kqc^__02_-^z(2#e@72@q@4$d4vv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
