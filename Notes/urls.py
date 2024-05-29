829d206b24a0aea15b9247955f561b3f024682d0
Mayank Kumar <87866647+mkkotlin@users.noreply.github.com>
2024-05-29 02:55:12 +0000

    modified

--- Notes/urls.py
+++ Notes/urls.py
@@ -5,7 +5,13 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('save',views.save),
     path('view_saved',views.view_saved,name='saved'),
-    path('get_by_title/<slug:slug>/',views.get_by_title,name='get_by_title'),
-    path('get_by_title/<slug:slug>/delete',views.delete,name='filter_view'),
-    path('get_by_title/<slug:slug>/update',views.update),
+    path('get_by_id/<int:id>/',views.get_by_id, name='get_by_id'),
+    path('get_by_id/<int:id>/delete',views.delete,name='filter_view'),
+    path('get_by_id/<int:id>/update',views.update),
 ]
+
+
+
+    # path('get_by_title/<slug:slug>/',views.get_by_title,name='get_by_title'),
+    # path('get_by_title/<slug:slug>/delete',views.delete,name='filter_view'),
+    # path('get_by_title/<slug:slug>/update',views.update),
--- Notes/views.py
+++ Notes/views.py
@@ -20,7 +20,7 @@ def save(request):
     if notes_form.is_valid():
         title = notes_form.cleaned_data['title']   #get specific field value
         new_title = notes_form.save(commit=False)  #prevent save
-        new_title.title = title.replace(' ','_').lower()  # modifing title
+        # new_title.title = title.replace(' ','_').lower()  # modifing title
         new_title.save()                           # saving all field after modification
         notes_form = CreateNotes()
         return render(request,'Notes.html',{'saved':'saved',"notes_form": notes_form})
@@ -32,26 +32,25 @@ def view_saved(request):
 
 
 
-def get_by_title(request,slug):
-    slug = slugify(slug)
-    x = get_list_or_404(Notes, title=slug)
-    url = reverse('get_by_title',kwargs={'slug':slug})
-    return render(request,'ViewSaved.html',{'x':x,'url':url})
 
-def delete(request,slug):
-    slug = slugify(slug)
-    x = get_list_or_404(Notes, title=slug)
-    ins = Notes.objects.filter(title=slug)
+
+def get_by_id(request,id):
+    print(id)
+    x = get_list_or_404(Notes,id=id)
+    return render(request,'ViewSaved.html',{'x':x})
+
+
+
+
+def delete(request,id):
+    ins = Notes.objects.filter(id=id)
     ins.delete()
     return redirect('saved')
 
-
-
-def update(request,slug):
-    slug = slugify(slug)
-    u = get_object_or_404(Notes,title=slug)
-    u.title = u.title.replace('_',' ')
-    u.title = u.title.lower()  #capture data and modifying
+def update(request,id):
+    u = get_object_or_404(Notes,id=id)
+    # u.title = u.title.replace('_',' ')
+    # u.title = u.title.lower()  #capture data and modifying
     if request.method == 'POST':
         notes_form = CreateNotes(request.POST,instance=u)
         if notes_form.is_valid():
@@ -59,7 +58,34 @@ def update(request,slug):
             return redirect('saved')
     else:
         notes_form = CreateNotes(instance=u)
-        notes_form.fields['title'].widget.attrs['readonly'] = True
+        # notes_form.fields['title'].widget.attrs['readonly'] = True  ##this is to set field editable/not
     return render(request,'update.html',{"notes_form": notes_form})
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Earlier Used
+# def get_by_title(request,slug):
+#     slug = slugify(slug)
+#     x = get_list_or_404(Notes, title=slug)
+#     url = reverse('get_by_title',kwargs={'slug':slug})
+#     return render(request,'ViewSaved.html',{'x':x,'url':url})
+
+# def delete(request,slug):
+#     slug = slugify(slug)
+#     x = get_list_or_404(Notes, title=slug)
+#     ins = Notes.objects.filter(title=slug)
+#     ins.delete()
+#     return redirect('saved')
